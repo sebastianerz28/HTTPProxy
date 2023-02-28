fec94a6cb012233f93941495de7179f9085cb8d3
Sebastian Ramirez <sebastianerz@gmail.com>
2023-02-27 21:39:48 -0700

    Add basic threading

--- HTTPproxy.py
+++ HTTPproxy.py
@@ -1,5 +1,6 @@
 # Place your imports here
 import signal, sys, socket, re
+from threading import *
 from optparse import OptionParser
 from urllib.parse import urlparse
 cache = {}
@@ -13,27 +14,53 @@ def ctrl_c_pressed(signal, frame):
 def main(addr, port):
     serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serverSock.bind((addr,port))
+    serverSock.listen()
+    print("Listening to requests")
     while True:
-        print("Listening to requests")
         clientSock, clientAddr= serverSock.accept()
-        print("Client:", clientAddr,"connected")
-        msg = clientSock.recv(1024)
-        valid = parseHTTPRequest(msg)
-        if valid['error'] is None:
-            outBoundRequest = buildRequest(valid)
+        thread = Thread(target=DoWork, args=(clientSock,clientAddr))
+        thread.run()
 
 
-        else:
-            clientSock.send(valid['error'])
+def DoWork(clientSock, clientAddr):
+    print("Client:", clientAddr, "connected")
+    msg = clientSock.recv(1024)
+    parsedInfo = parseHTTPRequest(msg)
+    if 'error' not in parsedInfo:
+        outBoundRequest = buildRequest(parsedInfo)
+        requestSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        requestSock.connect((parsedInfo['host'], 80))
+        notTimedOut = True
+        requestSock.send(outBoundRequest.encode())
+        data = []
+        while notTimedOut:
+            try:
+                currData = requestSock.recv(4096)
+                if len(currData) == 0:
+                    break
+                data.append(currData)
+            except:
+                notTimedOut = False
+        x = ""
+        for s in data:
+            x += s.decode()
+        clientSock.sendall(x.encode())
+        requestSock.close()
+
+    else:
+        clientSock.sendall(parsedInfo['error'].encode())
+    clientSock.close()
 
 
 
 
 def parseHTTPRequest(request):
+    #Check if request is a match to regex
     regex = re.compile(requestPattern, re.DOTALL)
-    regexMatch = regex.match(request)
+    regexMatch = regex.match(request.decode())
     if  regexMatch is not None:
         if 'GET' in regexMatch.group('method'):
+            #Get regex groups and add them to a dictionary containing everything needed to build a request
             return {'method': regexMatch.group('method'), 'url': urlparse(regexMatch.group('url'))[2], 'version': regexMatch.group('httpV'), 'headers': regexMatch.group('headers'),
                     'host': urlparse(regexMatch.group('url'))[1]}
         else:
@@ -44,8 +71,11 @@ def parseHTTPRequest(request):
 def buildRequest(valid):
     needsConnectionClose = True
     needsHost = True
+    #Add GET / <HTTPVERSION>
     request = "" + valid['method'].strip() + " " + valid['url'].strip() + " " + valid["version"].strip() + "\r\n"
+    #Split headers
     allHeaders = valid["headers"].splitlines()
+    #Check if we need a connection close header and hostname header
     for header in allHeaders:
         if header.strip() == "Connection: close":
             needsConnectionClose = False
@@ -56,6 +86,7 @@ def buildRequest(valid):
         request += "Host: " + valid['host'].strip() + "\r\n"
     if needsConnectionClose:
         request += "Connection: close\r\n"
+    #Add rest of the headers at the bottom
     for header in allHeaders:
         x = header.strip()
         if x != "" and x != " " and x != "\r" and x != "\r\n":
